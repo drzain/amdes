b4561aaf7731b6174d88f6f089be53559a9f6b6e
earth1991 <wahab.sukoraharjo@gmail.com>
2020-07-15 16:20:58 +0700

    berubah cin

--- app/src/main/java/com/sip/amdesmobile/LacakActivity.java
+++ app/src/main/java/com/sip/amdesmobile/LacakActivity.java
@@ -1,13 +1,32 @@
 package com.sip.amdesmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LacakActivity extends AppCompatActivity {
+public class LacakActivity extends AppCompatActivity
+{
+    Button btn_salesman_lacak;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lacak);
+
+        btn_salesman_lacak = findViewById(R.id.btn_salesman_lacak);
+        btn_salesman_lacak.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(LacakActivity.this, DetailLacakActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/sip/amdesmobile/SalesmanActivity.java
+++ app/src/main/java/com/sip/amdesmobile/SalesmanActivity.java
@@ -1,13 +1,32 @@
 package com.sip.amdesmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SalesmanActivity extends AppCompatActivity {
+public class SalesmanActivity extends AppCompatActivity
+{
 
+    Button btn_salesman_search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salesman);
+
+        btn_salesman_search = findViewById(R.id.btn_salesman_search);
+
+        btn_salesman_search.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+            Intent intent = new Intent(SalesmanActivity.this, LacakActivity.class);
+            startActivity(intent);
+            finish();
+            }
+
+        });
+
+
     }
 }
