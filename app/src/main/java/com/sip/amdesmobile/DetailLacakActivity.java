e1cf683fb8f420ff89c9a1f834de4dc3ffa9b20d
earth1991 <wahab.sukoraharjo@gmail.com>
2020-07-15 18:03:24 +0700

    berubah cin

--- app/src/main/java/com/sip/amdesmobile/DetailLacakActivity.java
+++ app/src/main/java/com/sip/amdesmobile/DetailLacakActivity.java
@@ -1,13 +1,29 @@
 package com.sip.amdesmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailLacakActivity extends AppCompatActivity {
 
+    Button btn_home;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_lacak);
+
+        btn_home = findViewById(R.id.btn_salesman_lacak_home);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailLacakActivity.this, SalesmanActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
