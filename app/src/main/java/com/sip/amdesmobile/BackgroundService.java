8d8f5ca235d0e73f1049d8717877d84235021830
drzain <duniaku2.ramdhani@gmail.com>
2020-07-26 12:43:28 +0700

    start driver

---
+++ app/src/main/java/com/sip/amdesmobile/BackgroundService.java
@@ -0,0 +1,235 @@
+package com.sip.amdesmobile;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BackgroundService extends Service {
+
+    private final LocationServiceBinder binder = new LocationServiceBinder();
+    private final String TAG = "AMDes Service";
+    private LocationListener mLocationListener;
+    private LocationManager mLocationManager;
+    private SessionManager session;
+    private NotificationManager notificationManager;
+
+    private final int LOCATION_INTERVAL = 500;
+    private final int LOCATION_DISTANCE = 10;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    private class LocationListener implements android.location.LocationListener
+    {
+        private Location lastLocation = null;
+        private final String TAG = "LocationListener";
+        private Location mLastLocation;
+
+        public LocationListener(String provider)
+        {
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            session = new SessionManager(getApplicationContext());
+            String userid = session.isUser();
+            mLastLocation = location;
+            Log.i(TAG, "LocationChanged: "+location);
+            JSONObject jsonObject = new JSONObject();
+
+            try {
+                jsonObject.put("secret_key", Constants.SECRET_KEY);
+                jsonObject.put("latitude", location.getLatitude());
+                jsonObject.put("longitude", location.getLongitude());
+                jsonObject.put("deviceID", getDeviceImei(BackgroundService.this));
+                Log.e("latitude",jsonObject.toString());
+
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String lat = Double.toString(latitude);
+                String longi = Double.toString(longitude);
+                String rahasia = Constants.SECRET_KEY;
+                String deviceid = getDeviceImei(BackgroundService.this);
+                String url = Constants.TRACK_URL;
+
+                kirim(lat,longi,rahasia,deviceid,url,userid);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider)
+        {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider)
+        {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+            Log.e(TAG, "onStatusChanged: " + status);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        Log.i(TAG, "onCreate");
+        startForeground(12345678, getNotification());
+    }
+
+    @SuppressLint("MissingPermission")
+    public static String getDeviceImei(Context ctx) {
+        TelephonyManager telephonyManager = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (mLocationManager != null) {
+            try {
+                mLocationManager.removeUpdates(mLocationListener);
+                stopForeground(true);
+            } catch (Exception ex) {
+                Log.i(TAG, "fail to remove location listners, ignore", ex);
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    public void startTracking() {
+        initializeLocationManager();
+        mLocationListener = new LocationListener(LocationManager.GPS_PROVIDER);
+
+        try {
+            mLocationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListener );
+
+        } catch (java.lang.SecurityException ex) {
+            // Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            // Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+
+    }
+
+    public void stopTracking() {
+        this.onDestroy();
+    }
+
+    private Notification getNotification() {
+
+        NotificationChannel channel = new NotificationChannel("channel_01", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        Notification.Builder builder = new Notification.Builder(getApplicationContext(), "channel_01").setAutoCancel(true);
+        return builder.build();
+    }
+
+
+    public class LocationServiceBinder extends Binder {
+        public BackgroundService getService() {
+            return BackgroundService.this;
+        }
+    }
+
+    private void kirim(final String lat, final String longi, final String rahasia, final String deviceid, final String url, final String userid){
+        String tag_string_req = "req_login";
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Login Response: " + response.toString());
+                //hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    Log.e(TAG, "respon "+response);
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                    //Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Volley Error: " + error.getMessage());
+                /*Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();*/
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("latitude", lat);
+                params.put("longitude", longi);
+                params.put("rahasia", rahasia);
+                params.put("deviceid", deviceid);
+                params.put("userid",userid);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+
+
+    }
+
+}
--- app/src/main/java/com/sip/amdesmobile/DriverActivity.java
+++ app/src/main/java/com/sip/amdesmobile/DriverActivity.java
@@ -1,7 +1,12 @@
 package com.sip.amdesmobile;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,9 +25,13 @@ import java.util.List;
 
 public class DriverActivity extends AppCompatActivity {
 
+    public BackgroundService gpsService;
+    public boolean mTracking = false;
     private RecyclerView recyclerView;
     private ArrayList<DataTask> driverArrayList;
     private ListAdapter adapter;
+    private SessionManager session;
+    Button btnStartTracking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +51,38 @@ public class DriverActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(DriverActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+
+        btnStartTracking = findViewById(R.id.btn_start_tracking);
+
+        final Intent intent = new Intent(this.getApplication(), BackgroundService.class);
+        this.getApplication().startService(intent);
+//        this.getApplication().startForegroundService(intent);
+        this.getApplication().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                btnStartTracking.performClick();
+            }
+        }, 1000);
     }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            String name = className.getClassName();
+            if (name.endsWith("BackgroundService")) {
+                gpsService = ((BackgroundService.LocationServiceBinder) service).getService();
+                btnStartTracking.setEnabled(true);
+                //txtStatus.setText("GPS Ready");
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            if (className.getClassName().equals("BackgroundService")) {
+                gpsService = null;
+            }
+        }
+    };
 
     @Override
     public boolean onSupportNavigateUp() {
